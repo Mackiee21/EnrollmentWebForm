616fd321c08618880bce9c4f10b781901fcfaba2
Mackiee21 <markpandac02@gmail.com>
2023-11-27 13:03:48 +0800

    fixed the header not sure if nindot naba

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -6,11 +6,13 @@ import Button from '../reusable/Button';
 function NavBar(_, ref){
     const classes = "shadow-md shadow-header/50 bg-header text-white";
     return(
-        <div ref={ref} className="flex align-center bg-gray-100 justify-between py-3 px-5 sticky top-0 z-50 
+        <div ref={ref} className="flex align-center bg-gray-100 justify-between py-2 px-5 sticky top-0 z-50 
         border-b-2">
-            <div className="font-bold text-xl uppercase text-main tracking-wide flex items-center gap-1">
-                <User size={22}  />
-                Mackiee
+            <div className="font-extrabold text-lg text-main tracking-wide flex items-center gap-1">
+                <div className='w-10 rounded-full border-2 border-gray-300'>
+                    <img className='w-full h-auto rounded-full object-cover object-center' src="https://scontent.fceb2-1.fna.fbcdn.net/v/t39.30808-6/299978409_2118215458348424_4457052968317549199_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeEIz21xspMn7cvotyf_NoCBcllEffGsjptyWUR98ayOm8lvkY5mjT7YVJSBjbAcw7dmiHrsXUfwhyApdD2udlB0&_nc_ohc=Nt6qWJNzb10AX-Q4vFY&_nc_ht=scontent.fceb2-1.fna&oh=00_AfDNybKOp4g8msi48rnSVqIj6APrZH67lxCUsitH1Oaaew&oe=656595FF" />
+                </div>
+               <a href='https://github.com/Mackiee21' target="_blank">@Mackiee</a>
             </div>
            <nav className='nav-header flex gap-2 items-center'>
                 <ul className="flex text-sm font-medium gap-5">
--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -7,37 +7,37 @@ function Sidebar({ mTop }){
                <nav className="py-2 px-2">
                     <ul className="tracking-wider">
                         <li> {/**wrapper nalng ni hahahhah */}
-                            <Link className='font-normal flex items-center gap-2 lg:hover:bg-neutral-200 py-2.5 px-2 rounded w-full' to="/development">
+                            <Link className='font-medium flex items-center gap-2 lg:hover:bg-neutral-200 hover:text-main py-2.5 px-2 rounded w-full' to="/development">
                                 <Home size={17} />
                                 Home
                             </Link>
                         </li>
                         <li>
-                            <Link className='font-normal flex items-center gap-2 lg:hover:bg-neutral-200 py-2.5 px-2 rounded' to="/">
+                            <Link className='font-medium flex items-center gap-2 lg:hover:bg-neutral-200 hover:text-main py-2.5 px-2 rounded' to="/">
                                 <UserPlusIcon size={17} />
                                 Students
                             </Link>
                         </li>
                         <li>
-                            <Link className='font-normal flex items-center gap-2 lg:hover:bg-neutral-200 py-2.5 px-2 rounded-md' to="/development">
+                            <Link className='font-medium flex items-center gap-2 lg:hover:bg-neutral-200 hover:text-main py-2.5 px-2 rounded-md' to="/development">
                                 <BookIcon size={17}  />
                                 Subjects
                             </Link>
                         </li>
                         <li>
-                            <Link className="font-normal flex items-start gap-2 lg:hover:bg-neutral-200 py-2.5 px-2 rounded" to="/development">
+                            <Link className="font-medium flex items-start gap-2 lg:hover:bg-neutral-200 hover:text-main py-2.5 px-2 rounded" to="/development">
                                 <SearchIcon size={18} />
                                 Prerequisites
                             </Link>
                         </li>
                         <li>
-                            <Link className="font-normal flex items-center gap-2 lg:hover:bg-neutral-200 py-2.5 px-2 rounded" to="/development">
+                            <Link className="font-medium flex items-center gap-2 lg:hover:bg-neutral-200 hover:text-main py-2.5 px-2 rounded" to="/development">
                                 <Calendar size={17} />
                                 Schedules
                             </Link>
                         </li>
                         <li>
-                            <Link className="font-normal flex items-center gap-2 lg:hover:bg-neutral-200 py-2.5 px-2 rounded" to="/development">
+                            <Link className="font-medium flex items-center gap-2 lg:hover:bg-neutral-200 hover:text-main py-2.5 px-2 rounded" to="/development">
                                 <FolderCheck size={17} />
                                 Enrollment
                             </Link>
